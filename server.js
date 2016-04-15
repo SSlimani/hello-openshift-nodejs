4130864c0775f6509377f9e98a67dc3a591baeb8
SSlimani <SSlimani@users.noreply.github.com>
2016-04-15 16:48:54 +0200

    test to trigger build 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from OpenShift - HelloWorld - Test OpenShift\n");
+  response.end("Hello from OpenShift - HelloWorld - Test OpenShift 2\n");
 }).listen(port);
 
 console.log("Server running on port " + port);
